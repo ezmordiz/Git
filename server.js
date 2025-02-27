015e81f4de1aacd50f2515aa7458b22a37a4859c
ezmordiz <ezmordiz@gmail.com>
2025-02-27 07:50:22 +0500

    1

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream
 // server.js
 import { createServer } from 'node:http';
 const server = createServer((req, res) => {
@@ -10,19 +9,3 @@ server.listen(3000, '127.0.0.1', () => {
   console.log('Listening on 127.0.0.1:3000');
 });
 // run with `node server.js`
-=======
-// server.mjs
-import { createServer } from 'node:http';
-
-const server = createServer((req, res) => {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello World!\n');
-});
-
-// starts a simple http server locally on port 3000
-server.listen(3000, '127.0.0.1', () => {
-    console.log('Listening on 127.0.0.1:3000');
-});
-
-// run with `node server.mjs`
->>>>>>> Stashed changes
